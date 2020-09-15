a679ed916e382207ec376c59e1a8f1a0b1d18804
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-09-15 20:20:13 +0300

    Updates

---
+++ precache-manifest.4a953e58a64a4503f5b14c14f5f6e484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a8dfa903f6f8fbef521cc0ca734e45",
+    "url": "/app-react-redux-api/index.html"
+  },
+  {
+    "revision": "bef421abbdbe5799a343",
+    "url": "/app-react-redux-api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ede7da1eb8ccf87fddf7",
+    "url": "/app-react-redux-api/static/js/2.c3ed815f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-react-redux-api/static/js/2.c3ed815f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef421abbdbe5799a343",
+    "url": "/app-react-redux-api/static/js/main.166b388d.chunk.js"
+  },
+  {
+    "revision": "f64b5dc660f254bac74c",
+    "url": "/app-react-redux-api/static/js/runtime-main.382f45c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app-react-redux-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
